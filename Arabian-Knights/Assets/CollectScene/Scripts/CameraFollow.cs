75d8fee2632fd1b159c1bbd9cee855bfe3b12bfe
ashwag1cs <ashwag1cs@gmail.com>
2026-02-17 00:14:58 +0300

    Level Design-V1

--- Arabian-Knights/Assets/CollectScene/Scripts/CameraFollow.cs
+++ Arabian-Knights/Assets/CollectScene/Scripts/CameraFollow.cs
@@ -1,19 +1,47 @@
 using UnityEngine;
-using static UnityEngine.GraphicsBuffer;
 
 public class CameraFollow : MonoBehaviour
 {
+    [SerializeField]
+    private Transform target;
+    [SerializeField]
+    private BoxCollider2D cameraBounds;
+
     private Vector3 offset = new Vector3(0f, 0f, -10f);
-    private float smoothTime = 0.25f;
-    private Vector3 velocity = Vector3.zero;
+    private Camera cam;
 
-    [SerializeField] private Transform target;
+    private float minX, maxX, minY, maxY;
 
-    // Update is called once per frame
-    void Update() {
-        Vector3 targetPosition = target.position + offset;
-        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+    void Awake()
+    {
+        cam = GetComponent<Camera>();
     }
 
+    void Start()
+    {
+        CalculateBounds();
+    }
 
+    void LateUpdate()
+    {
+        Vector3 targetPos = target.position + offset;
+
+        float clampedX = Mathf.Clamp(targetPos.x, minX, maxX);
+        float clampedY = Mathf.Clamp(targetPos.y, minY, maxY);
+
+        transform.position = new Vector3(clampedX, clampedY, offset.z);
+    }
+
+    void CalculateBounds()
+    {
+        Bounds bounds = cameraBounds.bounds;
+
+        float camHeight = cam.orthographicSize;
+        float camWidth = camHeight * cam.aspect;
+
+        minX = bounds.min.x + camWidth;
+        maxX = bounds.max.x - camWidth;
+        minY = bounds.min.y + camHeight;
+        maxY = bounds.max.y - camHeight;
+    }
 }
