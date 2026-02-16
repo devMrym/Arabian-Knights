5fc31bf2b46a8c8db03d4d84247c7d332cb16df7
Maryam_Alonazi <dev.mrym@gmail.com>
2026-02-16 19:35:20 +0300

    camera doesnt go beyond map

--- Arabian-Knights/Assets/FIghtScene/Scripts/CameraFollowFight.cs
+++ Arabian-Knights/Assets/FIghtScene/Scripts/CameraFollowFight.cs
@@ -4,11 +4,44 @@ public class CameraFollowFight : MonoBehaviour
 {
     [SerializeField]
     private Transform target;
+    [SerializeField]
+    private BoxCollider2D cameraBounds;
 
     private Vector3 offset = new Vector3(0f, 0f, -10f);
+    private Camera cam;
+
+    private float minX, maxX, minY, maxY;
+
+    void Awake()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+    void Start()
+    {
+        CalculateBounds();
+    }
 
     void LateUpdate()
     {
-        transform.position = target.position + offset;
+        Vector3 targetPos = target.position + offset;
+
+        float clampedX = Mathf.Clamp(targetPos.x, minX, maxX);
+        float clampedY = Mathf.Clamp(targetPos.y, minY, maxY);
+
+        transform.position = new Vector3(clampedX, clampedY, offset.z);
+    }
+
+    void CalculateBounds()
+    {
+        Bounds bounds = cameraBounds.bounds;
+
+        float camHeight = cam.orthographicSize;
+        float camWidth = camHeight * cam.aspect;
+
+        minX = bounds.min.x + camWidth;
+        maxX = bounds.max.x - camWidth;
+        minY = bounds.min.y + camHeight;
+        maxY = bounds.max.y - camHeight;
     }
 }
