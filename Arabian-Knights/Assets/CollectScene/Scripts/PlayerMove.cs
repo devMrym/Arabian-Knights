b9b53c9803fefeb7415db387354273426e1a0e3b
ashwag1cs <ashwag1cs@gmail.com>
2026-02-18 21:45:42 +0300

    Test

--- Arabian-Knights/Assets/CollectScene/Scripts/PlayerMove.cs
+++ Arabian-Knights/Assets/CollectScene/Scripts/PlayerMove.cs
@@ -2,15 +2,41 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    public float Speed = 5f;
+    public float moveSpeed = 5f;
+
+    private Rigidbody2D rb;
+    private Vector2 movement;
+    private PlayerAnimation anim;
+
+    public Vector2 facingDirection = Vector2.right;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.interpolation = RigidbodyInterpolation2D.Interpolate;
+        anim = GetComponent<PlayerAnimation>();
+    }
 
     void Update()
     {
-        float x = Input.GetAxisRaw("Horizontal");
-        float y = Input.GetAxisRaw("Vertical");
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        movement = movement.normalized;
 
-        Vector2 movement = new Vector2(x, y).normalized;
+        // LEFT / RIGHT facing
+        if (movement.x > 0.01f)
+            facingDirection = Vector2.right;
+        else if (movement.x < -0.01f)
+            facingDirection = Vector2.left;
+    }
 
-        transform.position += (Vector3)movement * Speed * Time.deltaTime;
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    public float GetMovementAmount()
+    {
+        return movement.magnitude;
     }
 }
